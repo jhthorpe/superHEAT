1f224c7f2d774c0fd095b441e1850c5a64618d29
jhthorpe <james.thorpe@utexas.edu>
2023-06-30 16:41:00 -0500

    added in some extra print options

--- superHEAT.py
+++ superHEAT.py
@@ -44,11 +44,13 @@
 
 #*************************************************************
 # DOCOPT input string
+# Please don't modify unless you know exactly what you are doing...
 """superHEAT
 
 Usage:
-    superHEAT.py --name=<name> --ZMAT=<zmat> --runfile=<run.sh> --recipe=<recipe>
+    superHEAT.py --name=<name> --ZMAT=<zmat> --runfile=<run.sh> --recipe=<recipe> 
     superHEAT.py --joblist [--recipe=<recipie>]
+    superHEAT.py --show [--basis] [--calcs]  [--ZMATopts] [--runopts] 
     superHEAT.py --recipes
     superHEAT.py (-h | --help)
     superHEAT.py --version
@@ -123,6 +125,10 @@ class Option_List:
         self.update('jobname'    , Option(abrv = 'JJJ'  , default = None       ) )
         self.update('jobid'      , Option(abrv = 'xxxxx', default = None       ) )
         self.update('xcfour'     , Option(abrv = 'XC4'  , default = None       ) )
+
+    def print(self):
+        for key, option in self.dict.items():
+            print("Option:", key, ", abrv:", option.abrv, ", default:", option.default)
         
 #*************************************************************
 # Basis_Set
@@ -147,6 +153,10 @@ class Basis_Set:
         self.short_name  = short_name
         self.zeta        = zeta
         self.style       = style
+
+    def print(self):
+        print("Proper Name:", self.proper_name, ", GENBAS name:", self.GENBAS_name, 
+              ", short_name:", self.short_name, ", zeta:", self.zeta, ", style:", self.style)
 
 #*************************************************************
 # Basis
@@ -245,23 +255,25 @@ BASIS.add( Basis_Set(proper_name='aug-cc-pCV8Z', GENBAS_name='AUG-PCV8Z', short_
 #
 class Calc:
     
-    def __init__(self, proper_name, ZMAT_name, short_name, rhf_cc, uhf_cc, rohf_cc):
+    #NOTE
+    # If you change or add parameters here, be sure to update Calcs.select member function
+    #   to match!!
+    def __init__(self, proper_name, ZMAT_name, short_name, rhf_cc, uhf_cc, rohf_cc, nbody):
         self.proper_name = proper_name
         self.ZMAT_name   = ZMAT_name
         self.short_name  = short_name
         self.rhf_cc      = rhf_cc
         self.uhf_cc      = uhf_cc
         self.rohf_cc     = rohf_cc         
+        self.nbody       = nbody
 
     def print(self):
-        print(proper_name, ZMAT_name, short_name, rhf_cc, uhf_cc, rohf_cc)
-
+        print(proper_name, ZMAT_name, short_name, rhf_cc, uhf_cc, rohf_cc, nbody)
 
 #*************************************************************
 # Calcs
 #
-# 
-#Calcs. Note that all start with c_ to indicate they are a claculation
+# Calcs class, which contains a dict of the various calcs contained. 
 #
 class Calcs:
 
@@ -282,44 +294,75 @@ class Calcs:
     def get(self, short_name):
         return seld.dict[short_name]
 
+    #this returns a new Calc object, with only the calcs that match those requested by the user
+    # for example:
+    #   t2_calcs = CALCS.select(nbody=2) 
+    # 
+    # NOTE: if a new parameter for the Calc class is added, it should also be updated here
+    def select(self, proper_name=None, ZMAT_name=None, short_name=None, rhf_cc=None, uhf_cc=None,
+               rohf_cc=None, nbody=None):
+        new_calcs = Calcs()
+        for key, calc in self.dict.items():
+            if ( proper_name != None ) and ( calc.proper_name != proper_name ): continue
+            if (   ZMAT_name != None ) and (   calc.ZMAT_name != ZMAT_name   ): continue
+            if (  short_name != None ) and (  calc.short_name != short_name  ): continue
+            if (      rhf_cc != None ) and (      calc.rhf_cc != rhf_cc      ): continue
+            if (      uhf_cc != None ) and (      calc.uhf_cc != uhf_cc      ): continue
+            if (     rohf_cc != None ) and (     calc.rohf_cc != rohf_cc     ): continue
+            if (       nbody != None ) and (       calc.nbody != nbody       ): continue
+            new_calcs.add(calc)
+        return new_calcs
+
 #Calcs global variable
 CALCS = Calcs()
 
 CALCS.add( Calc(proper_name = "SCF",          ZMAT_name = "SCF",                    short_name = "scf",
-                     rhf_cc = "NCC",             uhf_cc = "NCC",                       rohf_cc = "VCC") )
+                     rhf_cc = "NCC",             uhf_cc = "NCC",                       rohf_cc = "VCC",
+                      nbody = 1) )
     
 CALCS.add( Calc(proper_name = "MP2",          ZMAT_name = "MP2",                    short_name = "mp2",
-                     rhf_cc = "VCC",             uhf_cc = "VCC",                       rohf_cc = "VCC") )
+                     rhf_cc = "VCC",             uhf_cc = "VCC",                       rohf_cc = "VCC",
+                      nbody = 2) )
 
 CALCS.add( Calc(proper_name = "SDQ-MP4",      ZMAT_name = "SDQ-MP4",                short_name = "sdqmp4",
-                     rhf_cc = "VCC",             uhf_cc = "VCC",                       rohf_cc = "VCC") )
+                     rhf_cc = "VCC",             uhf_cc = "VCC",                       rohf_cc = "VCC",
+                      nbody = 2) )
 
 CALCS.add( Calc(proper_name = "CCSD",         ZMAT_name = "CCSD",                   short_name = "D", 
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC") )
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
+                      nbody = 2) )
 
 CALCS.add( Calc(proper_name = "CCSD(T)",      ZMAT_name = "CCSD(T)",                short_name = "pt", 
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC") )
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
+                      nbody = 3) )
 
 CALCS.add( Calc(proper_name = "CCSD(T)_L",    ZMAT_name = "CCSD(T)_L",              short_name = "ptL", 
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC") )
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
+                      nbody = 3) )
 
 CALCS.add( Calc(proper_name = "CCSDT",        ZMAT_name = "CCSDT",                  short_name = "T", 
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC") )
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC", 
+                      nbody = 3) )
 
 CALCS.add( Calc(proper_name = "CCSDT(Q)",     ZMAT_name = "CCSDT(Q)",               short_name = "pq", 
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC") )
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC", 
+                      nbody = 4) )
 
 CALCS.add( Calc(proper_name = "CCSDT(Q)_L",   ZMAT_name = "CCSDT(Q)_L",             short_name = "pqL", 
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC") )
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC", 
+                      nbody = 4) )
 
 CALCS.add( Calc(proper_name = "CCSDTQ",       ZMAT_name = "CCSDTQ",                 short_name = "Q", 
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC") )
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC", 
+                      nbody = 4) )
 
 CALCS.add( Calc(proper_name = "CCSDTQ(P)_L",  ZMAT_name = "CC(n-1)(n)_L, EXCITE=5", short_name = "ppL", 
-                     rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC") )
+                     rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC", 
+                      nbody = 5) )
 
 CALCS.add( Calc(proper_name = "CCSDTQP(H)_L", ZMAT_name = "CC(n-1)(n)_L, EXCITE=6", short_name = "phL", 
-                     rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC") )
+                     rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC", 
+                      nbody = 6) )
 
 #*************************************************************
 # ZMAT class
@@ -604,21 +647,22 @@ class Job_List:
 # Constraints
 #
 # A class that gathers constraints that define a unique calculation 
-
+#
 class Constraints:
     
     def __init__(self):
-        self.type  = ''
-        self.core  = ''
-        self.calc  = ''
-        self.basis = '' 
+        self.dict = {'type':None, #options: DBOC, SREL, NREE 
+                     'calc':None, #
+                     'basis':None}
+
+    
 
 
 #*************************************************************
 # Recipe
 #
-# A recipe is just a list of jobs that we want to execute
-
+# A recipe is just a list of jobs that we want to execute. 
+#
 class Recipe:
 
     #initialize with some list of jobs
@@ -667,6 +711,28 @@ if __name__ == '__main__':
         joblist = Job_List(None, None, None)
 
         joblist.print_names() 
+
+    elif args['--show']:
+        if args['--basis'   ]: 
+            print("Available Basis Sets")
+            BASIS.print()
+
+        if args['--calcs'   ]: 
+            print("Available Calculations")
+            CALCS.print()
+
+        if args['--ZMATopts']: 
+            print("Modifiable options in ZMAT")
+            zmatopts = Option_List()
+            zmatopts.set_ZMAT_default()
+            zmatopts.print()
+
+        if args['--runopts']: 
+            print("Modifiable options in runfile")
+            runopts = Option_List()
+            runopts.set_run_default()
+            runopts.print()
+            
 
     #generating job files
     else:
