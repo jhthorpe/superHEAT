e552563eea0a2651698105e019fed17bdbb1b132
jhthorpe <james.thorpe@utexas.edu>
2024-03-25 17:01:44 -0400

    added perturbative methods to calc.py, but did not test the uhf/rohf implementations

---
+++ examples/pt-survey/pt-survey.py
@@ -0,0 +1,158 @@
+#*************************************************************
+# pt-survery.py 
+#
+#       JHT, March 25, 2024, Dallas, TX
+#
+# Generates a series of tests for various perturbative methods
+# of treating T/Q in CC theory
+# 
+# The following is a list of global objects (in all caps)
+# that can be used to generate your joblist:
+# 
+# BASIS 	-- lists all basis functions, see basis.py
+# CALCS		-- lists all calculations, see calc.py
+# OPTIONS	-- lists default options, see option.py
+#
+# You should NEVER modify these globals, instead make a copy of them and modify from there
+#
+#*************************************************************
+
+#*************************************************************
+# DOCOPT input string
+#
+# To modify to your new script, ONLY change the name. 
+#
+"""ptsurvey
+
+Usage:
+    ptsurvey.py --name=<name> --ZMAT=<zmat> --runfile=<run.sh> 
+    ptsurvey.py --joblist 
+    ptsurvey.py --abrvs
+    ptsurvey.py (-h | --help)
+    ptsurvey.py --version
+
+Options:
+    --joblist     Print all predetermined jobs.
+    --abrvs       Print the list of default abbreviations
+    -h --help     Show this screen.
+    --version     Show version.
+
+"""
+
+#*************************************************************
+# Import statements. Those below are required. Import whatever
+# else you might need  
+#
+from docopt import docopt
+import copy
+import sys
+from basis import *
+from calc import * 
+from zmat import *
+from runscript import *
+from option import *
+from job import *
+from utility import *
+
+
+#*************************************************************
+# make_joblist 
+#
+# This is the function you (the user) modify to generate whatever
+# set of jobs you want constructed. The Job-id numbers will be 
+# constructed automatically for you, in the order that you 
+# append the jobs to the list
+#
+# NOTE: DO NOT modify any pre-defined global variables
+#
+
+ZMAT_OPTIONS.update('1_basis' , Option(abrv = '1YY', default = None      , description="First row basis"        ) )
+ZMAT_OPTIONS.update('2_basis' , Option(abrv = '2YY', default = None      , description="Second row basis"       ) )
+ZMAT_OPTIONS.update('3_basis' , Option(abrv = '3YY', default = None      , description="Third row basis"        ) )
+
+def make_joblist(molecule=None, zmat=None, run=None):
+    joblist = Joblist(molecule=molecule, zmat=zmat, run=run)
+
+    if (zmat != None):
+        ZMAT_OPTIONS.set('ref', zmat.get_ref().strip())
+
+    #Add some nonstandard options
+    ZMAT_OPTIONS.update('mem', Option(abrv = 'MMM', default = '118', description="memory"  ) )
+
+    RUN_OPTIONS.update('mem'     , Option(abrv = 'SMM'  , default = '128' , description="job memory") )
+    RUN_OPTIONS.update('cpu'     , Option(abrv = 'SPU'  , default = '32' , description="cpus") )
+    RUN_OPTIONS.update('xcfour', Option(abrv = 'XC4', default = 'source ~/settblis.sh\n    export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK\n    export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK\n    xjoda\n    xnmol\n    xnscf\n    xncc' , description="runs cfour"))
+    
+    #-------------------------------------
+    # all tests, we're using (Q)_L as the reference (well, potentially (P)_L...) 
+    # Note: (T-n), n=2,3,4,5 is all calculated at once
+    # Note: (Q-n), n=2,3,4,5,6 is all calculated at once
+    # Note: (TQ-n), n=2,3,4 is all calculated at once
+    # Note: (TQf) calculates (TQ), (TQf), +TQ and +TQ* all at once
+    # Note: (T)_L calculates (T) as well
+    # Note: (Q)_L calculates (Q) as well
+    # Note: (P)_L calculates (P) as well 
+    for c in ['D', 'pQL', 'Q', 'pPL', 'pTL', 'T-1', 'T-1b', 'T-2', 'T-3', 'T-4', 'pT-5', 'Q-1a', 'Q-1b', 'Q-3', 'pQ-6', 'pTQ-4', 'pTQ']
+    for s in ['DZ', 'TZ', 'QZ']
+        zopts = copy.deepcopy(ZMAT_OPTIONS)
+  
+        H_name = s.replace('C', '')
+
+        zopts.set('1_basis', BASIS.get(H_name).GENBAS_name)
+        zopts.set('2_basis', BASIS.get(s).GENBAS_name)
+        zopts.set('3_basis', BASIS.get(s).GENBAS_name)
+
+        #SET FROZEN CORE ON FOR THESE BASIS
+        zopts.set('frzcore', 'ON')
+
+        set_calc(CALCS.get(c), zopts)
+        set_basis(BASIS.get(s), zopts)
+
+        ropts = copy.deepcopy(RUN_OPTIONS)
+        ropts.set('jobname', molecule + c.lower() + "_" +  s.lower())
+
+        joblist.append(name=CALCS.get(c).proper_name + "/" + BASIS.get(s).proper_name,
+                       zmat_options = zopts,
+                       run_options  = ropts)
+
+    
+    return joblist
+
+
+#*************************************************************
+# Main function. Don't modify this except to rename your script 
+if __name__ == '__main__':
+
+    #Process Docopts input
+    args = docopt(__doc__, version="PT-survery 1.0")
+
+    #if printing joblist
+    if args['--joblist']:
+        joblist = make_joblist(molecule="")
+        joblist.print_names() #printing for now, save to file later
+
+    #print options
+    elif args['--abrvs']:
+        joblist = make_joblist(molecule="")
+
+        print("ZMAT variables")
+        ZMAT_OPTIONS.print()
+
+        print("run.dummy variables")
+        RUN_OPTIONS.print()
+
+    #generating job files
+    else:
+        #Read ZMAT
+        zmat = Zmat(args['--ZMAT'])
+
+        #Read run.dummy
+        rundummy = Runscript(args['--runfile'])
+
+        #Generate the basic joblist
+        joblist = make_joblist(args['--name'], zmat, rundummy)
+        joblist.print_names()
+
+        #generate the jobs with a particula
+        joblist.generate() #testing
+
--- src/calc.py
+++ src/calc.py
@@ -123,30 +123,6 @@ CALCS.add( Calc(proper_name = "CCSD",         ZMAT_name = "CCSD",               
                      rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
                       nbody = 2) )
 
-CALCS.add( Calc(proper_name = "CCSD(T)",      ZMAT_name = "CCSD(T)",                short_name = "pT",
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
-                      nbody = 3) )
-
-CALCS.add( Calc(proper_name = "CCSD(T)_L",    ZMAT_name = "CCSD(T)_L",              short_name = "pTL",
-                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
-                      nbody = 3) )
-
-CALCS.add( Calc(proper_name = "CCSDT",        ZMAT_name = "CCSDT",                  short_name = "T",
-                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "VCC",
-                      nbody = 3) )
-
-CALCS.add( Calc(proper_name = "CCSDT(Q)",     ZMAT_name = "CCSDT(Q)",               short_name = "pQ",
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
-                      nbody = 4) )
-
-CALCS.add( Calc(proper_name = "CCSDT(Q)_L",   ZMAT_name = "CCSDT(Q)_L",             short_name = "pQL",
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
-                      nbody = 4) )
-
-CALCS.add( Calc(proper_name = "CCSDTQ",       ZMAT_name = "CCSDTQ",                 short_name = "Q",
-                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
-                      nbody = 4) )
-
 CALCS.add( Calc(proper_name = "CCSDTQ(P)",    ZMAT_name = "CC(n-1)(n), EXCITE=5",   short_name = "pP",
                      rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC",
                       nbody = 5) )
@@ -163,8 +139,118 @@ CALCS.add( Calc(proper_name = "CCSDTQP(H)_L", ZMAT_name = "CC(n-1)(n)_L, EXCITE=
                      rhf_cc = "MRCC",            uhf_cc = "MRCC",                      rohf_cc = "MRCC",
                       nbody = 6) )
 
+# T3 methods
+CALCS.add( Calc(proper_name = "CCSD(T)",      ZMAT_name = "CCSD(T)",                short_name = "pT",
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSD(T)_L",    ZMAT_name = "CCSD(T)_L",              short_name = "pTL",
+                     rhf_cc = "NCC",             uhf_cc = "ECC",                       rohf_cc = "VCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT",        ZMAT_name = "CCSDT",                  short_name = "T",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "VCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT-1",      ZMAT_name = "CCSDT-1",                short_name = "T-1",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT-1b",      ZMAT_name = "CCSDT-1b",                short_name = "T-1b",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT-2",      ZMAT_name = "CCSDT-2",                short_name = "T-2",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT-3",      ZMAT_name = "CCSDT-3",                short_name = "T-3",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSDT-4",      ZMAT_name = "CCSDT-4",                short_name = "T-4",
+                     rhf_cc = "VCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSD(T-2)",      ZMAT_name = "CCSD(T-2)",               short_name = "pT-2",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSD(T-3)",      ZMAT_name = "CCSD(T-3)",               short_name = "pT-3",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSD(T-4)",      ZMAT_name = "CCSD(T-4)",               short_name = "pT-4",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+CALCS.add( Calc(proper_name = "CCSD(T-5)",      ZMAT_name = "CCSD(T-5)",               short_name = "pT-5",
+                     rhf_cc = "NCC",             uhf_cc = "VCC",                       rohf_cc = "MRCC",
+                      nbody = 3) )
+
+# T4 methods
+CALCS.add( Calc(proper_name = "CCSDT(Q)",     ZMAT_name = "CCSDT(Q)",               short_name = "pQ",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q)_L",   ZMAT_name = "CCSDT(Q)_L",             short_name = "pQL",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDTQ",       ZMAT_name = "CCSDTQ",                 short_name = "Q",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDTQ-1a",       ZMAT_name = "CCSDTQ-1a",                 short_name = "Q-1a",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDTQ-1b",       ZMAT_name = "CCSDTQ-1b",                 short_name = "Q-1b",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDTQ-3",       ZMAT_name = "CCSDTQ-3",                 short_name = "Q-3",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q-2)",       ZMAT_name = "CCSDT(Q-2)",                 short_name = "pQ-2",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q-3)",       ZMAT_name = "CCSDT(Q-3)",                 short_name = "pQ-3",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q-4)",       ZMAT_name = "CCSDT(Q-4)",                 short_name = "pQ-4",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q-5)",       ZMAT_name = "CCSDT(Q-5)",                 short_name = "pQ-5",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSDT(Q-6)",       ZMAT_name = "CCSDT(Q-6)",                 short_name = "pQ-6",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+#simul T3 T3 methods
 CALCS.add( Calc(proper_name = "CCSD(TQ)",     ZMAT_name = "CCSD(TQ)",               short_name = "pTQ",
                      rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
                       nbody = 4) )
 
+CALCS.add( Calc(proper_name = "CCSD(TQf)",     ZMAT_name = "CCSD(TQf)",               short_name = "pTQf",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSD(TQ-2)",     ZMAT_name = "CCSD(TQ-2)",               short_name = "pTQ-2",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSD(TQ-3)",     ZMAT_name = "CCSD(TQ-3)",               short_name = "pTQ-3",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
+
+CALCS.add( Calc(proper_name = "CCSD(TQ-4)",     ZMAT_name = "CCSD(TQ-4)",               short_name = "pTQ-4",
+                     rhf_cc = "NCC",             uhf_cc = "MRCC",                      rohf_cc = "MRCC",
+                      nbody = 4) )
 
